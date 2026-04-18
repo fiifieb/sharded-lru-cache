8d70fd59cdaec79dba43cf8f8946102b55642dd0
Fiifi Botchway <feb49@cornell.edu>
2026-04-17 23:07:28 -0700

    Step 2: implement lock-safe shard core

--- include/sharded_lru/detail/lru_list.hpp
+++ include/sharded_lru/detail/lru_list.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <list>
+#include <stdexcept>
 #include <unordered_map>
 
 namespace sharded_lru::detail {
@@ -11,10 +12,36 @@ class LruList {
   using ListType = std::list<Key>;
   using Iterator = typename ListType::iterator;
 
-  void touch(const Key& key);
-  void remove(const Key& key);
-  const Key& evict_candidate() const;
-  std::size_t size() const;
+  void touch(const Key& key) {
+    const auto it = index_.find(key);
+    if (it != index_.end()) {
+      order_.splice(order_.begin(), order_, it->second);
+      it->second = order_.begin();
+      return;
+    }
+
+    order_.push_front(key);
+    index_.emplace(*order_.begin(), order_.begin());
+  }
+
+  void remove(const Key& key) {
+    const auto it = index_.find(key);
+    if (it == index_.end()) {
+      return;
+    }
+
+    order_.erase(it->second);
+    index_.erase(it);
+  }
+
+  const Key& evict_candidate() const {
+    if (order_.empty()) {
+      throw std::out_of_range("cannot evict from empty LRU list");
+    }
+    return order_.back();
+  }
+
+  std::size_t size() const { return order_.size(); }
 
  private:
   ListType order_;
--- include/sharded_lru/detail/shard.hpp
+++ include/sharded_lru/detail/shard.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <functional>
+#include <mutex>
 #include <optional>
 #include <shared_mutex>
 #include <unordered_map>
+#include <utility>
 
 #include "sharded_lru/detail/lru_list.hpp"
 
@@ -13,14 +16,79 @@ namespace sharded_lru::detail {
 template <typename Key, typename Value, typename Hasher>
 class Shard {
  public:
-  Shard(std::size_t capacity, Hasher hasher = Hasher{});
+  Shard(std::size_t capacity, Hasher hasher = Hasher{})
+      : capacity_(capacity), hasher_(std::move(hasher)), items_(0, hasher_) {}
 
-  bool put(const Key& key, Value value);
-  std::optional<Value> get(const Key& key);
-  bool erase(const Key& key);
-  bool contains(const Key& key) const;
-  void clear();
-  std::size_t size() const;
+  bool put(const Key& key, Value value) {
+    std::unique_lock lock(mutex_);
+
+    if (capacity_ == 0) {
+      return false;
+    }
+
+    const auto found = items_.find(key);
+    if (found != items_.end()) {
+      found->second = std::move(value);
+      lru_.touch(key);
+      assert(items_.size() == lru_.size());
+      return false;
+    }
+
+    items_.emplace(key, std::move(value));
+    lru_.touch(key);
+
+    if (items_.size() > capacity_) {
+      const Key evict_key = lru_.evict_candidate();
+      lru_.remove(evict_key);
+      items_.erase(evict_key);
+    }
+
+    assert(items_.size() == lru_.size());
+    assert(items_.size() <= capacity_);
+    return true;
+  }
+
+  std::optional<Value> get(const Key& key) {
+    std::unique_lock lock(mutex_);
+    const auto found = items_.find(key);
+    if (found == items_.end()) {
+      return std::nullopt;
+    }
+
+    lru_.touch(key);
+    assert(items_.size() == lru_.size());
+    return found->second;
+  }
+
+  bool erase(const Key& key) {
+    std::unique_lock lock(mutex_);
+    const auto found = items_.find(key);
+    if (found == items_.end()) {
+      return false;
+    }
+
+    items_.erase(found);
+    lru_.remove(key);
+    assert(items_.size() == lru_.size());
+    return true;
+  }
+
+  bool contains(const Key& key) const {
+    std::shared_lock lock(mutex_);
+    return items_.find(key) != items_.end();
+  }
+
+  void clear() {
+    std::unique_lock lock(mutex_);
+    items_.clear();
+    lru_ = LruList<Key>{};
+    assert(items_.size() == lru_.size());
+  }
+
+  std::size_t size() const {
+    std::shared_lock lock(mutex_);
+    return items_.size();
+  }
 
  private:
   std::size_t capacity_;
